c3cb41fc6b8dacc55ad6303c1f24503042608ae5
arthurtmotta <trindademotta.arthur@gmail.com>
2026-04-29 16:50:57 -0300

    4 commit

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter> basename="/timeless-treasures/"
+      <BrowserRouter basename="/timeless-treasures">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
