be3a411cdb9ec7fb75f1e566352dcd0dd4a8c12b
arthurtmotta <trindademotta.arthur@gmail.com>
2026-04-29 16:40:08 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter> basename="/timeless-tresures/"
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,40 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
+// export default defineConfig(({ mode }) => ({
+//   server: {
+//     host: "::",
+//     port: 8080,
+//     hmr: {
+//       overlay: false,
+//     },
+//   },
+//   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//     dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
+//   },
+
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
-  },
+ base: '/timeless-tresures/',
+ server: {
+ host: "::",
+ port: 8080,
+ hmr: {
+ overlay: false,
+ },
+ },
+ plugins: [react(), mode === "development" &&
+componentTagger()].filter(Boolean),
+ resolve: {
+ alias: {
+ "@": path.resolve(__dirname, "./src"),
+ },
+ dedupe: ["react", "react-dom", "react/jsx-runtime",
+"react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
+ },
 }));
+
+
