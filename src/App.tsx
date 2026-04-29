8a4919c996aafb8fc38eacae337c7de95d59461d
arthurtmotta <trindademotta.arthur@gmail.com>
2026-04-29 16:45:40 -0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter> basename="/timeless-tresures/"
+      <BrowserRouter> basename="/timeless-treasures/"
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ import { componentTagger } from "lovable-tagger";
 //   },
 
 export default defineConfig(({ mode }) => ({
- base: '/timeless-tresures/',
+ base: '/timeless-treasures/',
  server: {
  host: "::",
  port: 8080,
